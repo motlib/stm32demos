06b4cb04f7f1bc7aecb700d875335a8a1d1d8859
Andreas Schroeder <andreas@a-netz.de>
2017-03-06 21:54:06 +0800

    First working version.

--- minimal_1/main.c
+++ minimal_1/main.c
@@ -15,7 +15,7 @@
 #define SRAM_END        (SRAM_BASE + SRAM_SIZE)
 
 /* LED connected to PIN 8 of GPIOA */
-#define LED_PIN         12
+#define LED_PIN         0
 /* output mode: push-pull + 50MHz */
 #define OUTPUT_MODE     (0x10|0x03) 
 
@@ -34,13 +34,27 @@
 #define GPIOB_BASE      (PERIPH_BASE + 0x10C00)
 #define GPIOB_CRL       (*(volatile unsigned long*)(GPIOB_BASE + 0x00))
 #define GPIOB_CRH       (*(volatile unsigned long*)(GPIOB_BASE + 0x04))
-#define GPIOB_ODR       (*(volatile unsigned long*)(GPIOA_BASE + 0x0C))
+#define GPIOB_ODR       (*(volatile unsigned long*)(GPIOB_BASE + 0x0C))
 #define GPIOB_BSRR      (*(volatile unsigned long*)(GPIOB_BASE + 0x10))
 #define GPIOB_BRR       (*(volatile unsigned long*)(GPIOB_BASE + 0x14))
 
+#define STACK_TOP 0x20000800   // just a tiny stack for demo
+
+
 /* user functions */
+void delay();
+static void nmi_handler(void);
+static void hardfault_handler(void);
 int main(void);
-void delay(unsigned long count);
+
+// Define the vector table
+unsigned int *myvectors[4] __attribute__ ((section("vectors"))) = {
+    (unsigned int *) STACK_TOP,         // stack pointer
+    (unsigned int *) main,              // code entry point
+    (unsigned int *) nmi_handler,       // NMI handler (not really)
+    (unsigned int *) hardfault_handler  // hard fault handler
+};
+
 
 int main()
 {
@@ -51,27 +65,42 @@ int main()
     
 
     /* set LED pin output mode */
-    /*
-    GPIOA_CRL |= OUTPUT_MODE << ((LED_PIN) << 2); // if pins 0 to 7
-    GPIOB_CRH |= OUTPUT_MODE << ((LED_PIN-8) << 2); // if pins 8 to 15
-    */
+    
+//    GPIOA_CRL |= OUTPUT_MODE << ((LED_PIN) << 2); // if pins 0 to 7
+//    GPIOA_CRL |= 
+    //GPIOB_CRH |= OUTPUT_MODE << ((LED_PIN-8) << 2); // if pins 8 to 15
+    
+    GPIOA_CRL = (0x2 << 0u); // MODE: 0x2 = 2MHz
+    //GPIOA_CRL |= (0x0 << 18u); // CNF: 0x0 = general purpose push / pull
 
-    GPIOB_CRH |= (0x2 << 16u); // MODE: 0x2 = 2MHz
-    GPIOB_CRH |= (0x0 << 18u); // CNF: 0x0 = general purpose push / pull
+    //GPIOB_CRH |= (0x2 << 16u); // MODE: 0x2 = 2MHz
+    //GPIOB_CRH |= (0x0 << 18u); // CNF: 0x0 = general purpose push / pull
     
     while(1)
     {
-	GPIOB_ODR = (1u << LED_PIN);  // set LED pin high
-	delay(200000);
+	GPIOA_ODR |= (1u << LED_PIN);  // set LED pin high
+	delay();
 	
-	GPIOB_ODR &= ~(1u << LED_PIN);  // clear LED pin (low)
-	delay(200000);
+	GPIOA_ODR &= ~(1u << LED_PIN);  // clear LED pin (low)
+	delay();
     }
 }
 
-void delay(unsigned long count)
+
+void delay(void)
 {
-    volatile uint32_t c = count;
-    
-    while(c--);
+    volatile unsigned int i;
+
+    for (i = 0; i < 20000; i++)
+	;
+}
+
+void nmi_handler(void)
+{
+    for(;;);
+}
+
+void hardfault_handler(void)
+{
+    for(;;);
 }
